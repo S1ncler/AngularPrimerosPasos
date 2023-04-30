1e66aa205fe1caea9ff615da6516e4702f7ed860
S1ncler <daniel_felipe_c11@hotmail.com>
2023-04-30 18:08:58 -0500

    Subida

---
+++ src/app/app.cards.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cards',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppCards {
+  title = 'AngularPrimerosPasos';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppCards } from './app.cards';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppCards
   ],
   imports: [
     BrowserModule
